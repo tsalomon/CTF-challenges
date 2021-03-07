e150b303a1316b7d93e1333f079914ca9b6e0fe2
Tim Salomonsson <tsalomon@users.noreply.github.com>
2021-03-07 00:57:08 -0800

    Create baby_re.c

---
+++ baby_re.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+
+    long local_48 = 0x594234427b425448;
+    long local_40 = 0x3448545f5633525f;
+    int local_38 = 0x455f5354;
+    int local_34 = 0x7d5a;
+    
+    printf((char *)&local_48);
+    //puts((char *)&local_40);
+    printf((char *)&local_38);
+    //puts((char *)&local_34);
+
+
+    return 0;
+}
